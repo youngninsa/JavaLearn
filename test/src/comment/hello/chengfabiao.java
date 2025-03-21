2c9e627fe415913eb34ab3aeabb7ae067b772ece
100835 <112314721+100835@users.noreply.github.com>
2025-03-21 13:07:14 +0800

    九九乘法表

---
+++ test/src/comment/hello/chengfabiao.java
@@ -0,0 +1,24 @@
+package comment.hello;
+
+public class chengfabiao
+{
+    public static void main(String[] args)
+    {
+
+        for(int i=1;i<=9;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.printf("%d*%d=%d\t",j,i,i*j);
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for(int i=9;i>=1;i--)
+        {
+            for(int j=1;j<=i;j++)
+                System.out.printf("%d*%d=%d\t",j,i,i*j);
+            System.out.println();
+        }
+    }
+}
